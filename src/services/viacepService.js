18d19a03634a560848224f19a3b4ef4b16126767
Jonnathan Vituriano <viturianojonnathan@gmail.com>
2024-10-23 11:06:22 -0300

    🐛 fix: linha 6 não estava coincidindo com o código do viacepService.js

--- src/services/viacepService.js
+++ src/services/viacepService.js
@@ -3,7 +3,7 @@ const axios = require ('axios');
 async function buscarEndereco(cep) {
     try {
         const response = await axios.get (`https://viacep.com.br/ws/${cep}/json/`);
-        return responde.data;
+        return response.data;
     } catch (error) {
         console.error('Erro ao buscar o endereço: ', error);
         throw error;
