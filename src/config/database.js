207477d7b2bb2bcc762b651268c369dac68c878a
Jonnathan Vituriano <viturianojonnathan@gmail.com>
2024-10-24 09:19:14 -0300

    :sparkles: feat: Implementação dotEnv + database.js refactor

--- src/config/database.js
+++ src/config/database.js
@@ -1,7 +1,8 @@
+require('dotenv').config();
 const mongoose =  require('mongoose');
 
 function conectarMongoDB() {
-    mongoose.conect('URL AQUI!!', {
+    mongoose.conect(process.env.MONGO_URI, {
         useNewUrlParser: true,
         useUnifiedTopology: true
     })
